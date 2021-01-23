93eb0eed05c1dc8a1a10cb042c16c9d9c18e3c8f
DCStillz <dylan@dcokerstills.com>
2021-01-23 16:58:48 -0500

    commit tree eea31201a8b1ae5260287bffa98035e33794a576

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,6 @@
 class Dog
-    def initialize(breed)
-        @breed = breed
+    def initialize(name, breed="Mutt")
+      @name = name
+      @breed = breed
     end
-    def breed=(breed)
-        @breed = breed
-    end
-    def breed
-        @breed
-    end
-end
+  end
